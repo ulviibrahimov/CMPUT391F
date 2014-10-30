2cfb564f5cb18e08bb1769d5f5b52a6562f81c26
LindsayL <livojevi@ualberta.ca>
2014-10-30 15:03:53 -0600

    Helper function to read html files
    upload servlet displays UploadPage, yay. :)

--- WEB-INF/classes/Upload.java
+++ WEB-INF/classes/Upload.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ResourceBundle;
@@ -13,45 +16,14 @@ public class Upload extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        ResourceBundle rb =
-            ResourceBundle.getBundle("LocalStrings",request.getLocale());
         response.setContentType("text/html");
         response.setCharacterEncoding("UTF-8");
         PrintWriter out = response.getWriter();
 
-        String title = rb.getString("helloworld.title");
+        String html = UtilHelper.readHtmlFile("UploadPage.html");
 
-        // String html = "";
-        // try {
-        //     html = UtilHelper.readFileToString("../../html/UploadPage.html");
-        // } catch (Exception e) {
-        //     System.err.println(e);
-        // }
-// 
-        // out.println(html);
-
-        out.println("<!DOCTYPE html><html>");
-        out.println("<head>");
-        out.println("<meta charset=\"UTF-8\" />");
-
-        out.println("<title>" + title + "</title>");
-        out.println("</head>");
-        out.println("<body bgcolor=\"white\">");
-
-        // note that all links are created to be relative. this
-        // ensures that we can move the web application that this
-        // servlet belongs to to a different place in the url
-        // tree and not have any harmful side effects.
-
-        // XXX
-        // making these absolute till we work out the
-        // addition of a PathInfo issue
-
-        out.println("<h1>" + "welcome" + title + "</h1>");
-        out.println("</body>");
-        out.println("</html>");
+        out.println(html);
     }
-
 }
 
 
--- WEB-INF/classes/UtilHelper.java
+++ WEB-INF/classes/UtilHelper.java
@@ -4,16 +4,31 @@ import java.util.*;
 
 public class UtilHelper {
 
-    public static String readFileToString(String fileName) throws Exception {
+    /**
+     * Reads a file from the html directory and returns a String.
+     * Outputs an html formatted error in the case of an exception.
+     */
+    public static String readHtmlFile(String fileName) {
+        String out = "";
+        try {
+            out = readFile("webapps/CMPUT391F/html/"+fileName);    
+        } catch (Exception e){
+            out = "</p>404 - Failed to read resource: " + (new File("webapps/CMPUT391F/html/"+fileName)).getAbsolutePath() + "<br>" + e;
+        }
+        return out;
+    }
+
+    /**
+     * Reads a file relative the Tomcat directory and returns a String.
+     */
+    public static String readFile(String fileName) throws Exception {
         File inFile = new File(fileName);
         String out = "";
         try (Scanner scanner = new Scanner(new FileInputStream(inFile))){
             out = scanner.useDelimiter("\\Z").next();
-        }
-        catch (Exception e){
+        } catch (Exception e){
             throw e;
         }
-        
         return out;
     }
 }
