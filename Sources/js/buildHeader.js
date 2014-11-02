d3be4fad0fa9ed7ae5e605af3ae8922350b6b91a
LindsayL <livojevi@ualberta.ca>
2014-11-02 00:23:57 -0600

    Fixed some minor style problems
    Also added a template for search

--- Sources/js/buildHeader.js
+++ Sources/js/buildHeader.js
@@ -1,21 +1,19 @@
 "use strict";  // Makes bad syntax into actual errors
 
 $(document).ready(function() {
-	$('body').prepend('<div id="header"></div>');
+	$('body').prepend( 
+		'<div id="header">'
+			+'<div id="header-upper">'
+				+'<a href="/CMPUT391F"><img class="logo" src="/CMPUT391F/Sources/images/lux.png"></a>'
+				+'<a href="/CMPUT391F"><img class="logo" src="/CMPUT391F/Sources/images/ih.png"></a>'
+			+'</div>'
 
-	$("#header").append(
-		'<div id="header-upper">'
-		+'<a href="/CMPUT391F"><img class="logo" src="/CMPUT391F/Sources/images/lux.png"></a>'
-		+'<a href="/CMPUT391F"><img class="logo" src="/CMPUT391F/Sources/images/ih.png"></a>'
-		+'</div>'
-		);
-
-	$("#header").append(
-		'<div id="header-lower"><div class="nav-options">'
-		+'<div class="nav-option"><a href="/CMPUT391F">Home</a></div>'
-		+'<div class="nav-option"><a href="/CMPUT391F/upload.html">Upload</a></div>'
-		+'<div class="nav-option"><a href="/CMPUT391F">Search</a></div>'
-		+'<div class="nav-option"><a href="/CMPUT391F/login.html">Login</a></div>'
-		+'</div></div>'
-		);
+			+'<div id="header-lower"><div class="nav-options">'
+				+'<div class="nav-option"><a href="/CMPUT391F">Home</a></div>'
+				+'<div class="nav-option"><a href="/CMPUT391F/myimages.html">My Images</a></div>'
+				+'<div class="nav-option"><a href="/CMPUT391F/upload.html">Upload</a></div>'
+				+'<div class="nav-option"><a href="/CMPUT391F/search.html">Search</a></div>'
+				+'<div class="nav-option"><a href="/CMPUT391F/login.html">Login</a></div>'
+		+'</div></div></div>'
+	);
 });
