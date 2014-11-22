16158fda3dbc035b0c55808fe98917347dc397fd
ulviibrahimov <ulvi@ualberta.ca>
2014-11-22 14:16:18 -0700

    single sql file

--- setup.sql
+++ setup.sql
@@ -4,6 +4,7 @@
  *              Fall, 2014
  *  Author:     Prof. Li-Yan Yuan
  */
+DROP TABLE viewed;
 DROP TABLE images;
 DROP TABLE group_lists;
 DROP TABLE groups;
@@ -72,7 +73,7 @@ CREATE TABLE images (
 INSERT INTO users values('admin', 'secret', SYSDATE);
 INSERT INTO persons values('admin', null, null, null, null, null);
 
-DROP TABLE viewed;
+
 CREATE TABLE viewed (
    photo_id    int,
    name  varchar(24),
@@ -86,3 +87,142 @@ CREATE SEQUENCE pic_id_seq;
 
 DROP SEQUENCE group_id_seq;
 CREATE SEQUENCE group_id_seq START WITH 3;
+
+DROP INDEX myindex1;
+DROP INDEX myindex2;
+DROP INDEX myindex3;
+
+CREATE INDEX myindex1 ON images(subject) INDEXTYPE IS CTXSYS.CONTEXT;
+CREATE INDEX myindex2 ON images(place) INDEXTYPE IS CTXSYS.CONTEXT;
+CREATE INDEX myindex3 ON images(description) INDEXTYPE IS CTXSYS.CONTEXT;
+
+REM
+Rem Copyright (c) 1999, 2001, Oracle Corporation. All rights reserved.
+Rem
+Rem NAME
+Rem drjobdml.sql - DR dbms_JOB DML script
+Rem
+Rem NOTES
+Rem This is a script which demonstrates how to submit a DBMS_JOB
+Rem which will keep a context index up-to-date.
+Rem
+Rem Before running this script, please ensure that your database
+Rem is set up to run dbms_jobs. job_queue_processes must be set
+Rem (and non-zero) in init.ora. See Administrator's Guide for more
+Rem information.
+Rem
+Rem Also, due to PL/SQL security, ctxsys must manually grant EXECUTE
+Rem on ctx_ddl directly to the user. CTXAPP role is not sufficient.
+Rem
+Rem USAGE
+Rem as index owner, in SQL*Plus:
+Rem
+Rem @drjobdml <indexname> <interval>
+Rem
+Rem A job will be submitted to check for and process dml for the
+Rem named index every <interval> minutes
+Rem
+Rem MODIFIED (MM/DD/YY)
+Rem gkaminag 11/19/01 - bug 2052473.
+Rem gkaminag 05/14/99 - bugs
+Rem gkaminag 04/09/99 - Creation
+define idxname = "myindex1"
+define interval = "1"
+set serveroutput on
+declare
+job number;
+begin
+dbms_job.submit(job, 'ctx_ddl.sync_index(''&idxname'');',
+interval=>'SYSDATE+&interval/1440');
+commit;
+dbms_output.put_line('job '||job||' has been submitted.');
+end;
+/
+
+
+REM
+Rem Copyright (c) 1999, 2001, Oracle Corporation. All rights reserved.
+Rem
+Rem NAME
+Rem drjobdml.sql - DR dbms_JOB DML script
+Rem
+Rem NOTES
+Rem This is a script which demonstrates how to submit a DBMS_JOB
+Rem which will keep a context index up-to-date.
+Rem
+Rem Before running this script, please ensure that your database
+Rem is set up to run dbms_jobs. job_queue_processes must be set
+Rem (and non-zero) in init.ora. See Administrator's Guide for more
+Rem information.
+Rem
+Rem Also, due to PL/SQL security, ctxsys must manually grant EXECUTE
+Rem on ctx_ddl directly to the user. CTXAPP role is not sufficient.
+Rem
+Rem USAGE
+Rem as index owner, in SQL*Plus:
+Rem
+Rem @drjobdml <indexname> <interval>
+Rem
+Rem A job will be submitted to check for and process dml for the
+Rem named index every <interval> minutes
+Rem
+Rem MODIFIED (MM/DD/YY)
+Rem gkaminag 11/19/01 - bug 2052473.
+Rem gkaminag 05/14/99 - bugs
+Rem gkaminag 04/09/99 - Creation
+define idxname = "myindex2"
+define interval = "1"
+set serveroutput on
+declare
+job number;
+begin
+dbms_job.submit(job, 'ctx_ddl.sync_index(''&idxname'');',
+interval=>'SYSDATE+&interval/1440');
+commit;
+dbms_output.put_line('job '||job||' has been submitted.');
+end;
+/
+
+
+REM
+Rem Copyright (c) 1999, 2001, Oracle Corporation. All rights reserved.
+Rem
+Rem NAME
+Rem drjobdml.sql - DR dbms_JOB DML script
+Rem
+Rem NOTES
+Rem This is a script which demonstrates how to submit a DBMS_JOB
+Rem which will keep a context index up-to-date.
+Rem
+Rem Before running this script, please ensure that your database
+Rem is set up to run dbms_jobs. job_queue_processes must be set
+Rem (and non-zero) in init.ora. See Administrator's Guide for more
+Rem information.
+Rem
+Rem Also, due to PL/SQL security, ctxsys must manually grant EXECUTE
+Rem on ctx_ddl directly to the user. CTXAPP role is not sufficient.
+Rem
+Rem USAGE
+Rem as index owner, in SQL*Plus:
+Rem
+Rem @drjobdml <indexname> <interval>
+Rem
+Rem A job will be submitted to check for and process dml for the
+Rem named index every <interval> minutes
+Rem
+Rem MODIFIED (MM/DD/YY)
+Rem gkaminag 11/19/01 - bug 2052473.
+Rem gkaminag 05/14/99 - bugs
+Rem gkaminag 04/09/99 - Creation
+define idxname = "myindex3"
+define interval = "1"
+set serveroutput on
+declare
+job number;
+begin
+dbms_job.submit(job, 'ctx_ddl.sync_index(''&idxname'');',
+interval=>'SYSDATE+&interval/1440');
+commit;
+dbms_output.put_line('job '||job||' has been submitted.');
+end;
+/
