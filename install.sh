40f0556f06101780d2f6c21d249708b1665e52f6
xiaolu1 <xiaolu1@ualberta.ca>
2014-11-26 17:35:26 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,14 @@ for ((i=1;; i++)); do
     read "d$i" || break;
 done < $1
 
+echo "username $d9"
+echo "username $d10"
 
 sqlplus $d9/$d10<<THEEND
 
 -- Change "1" to the desired fatal return code
 
-whenever sqlerror exit 1;
+--whenever sqlerror exit 1;
 
 @setupscript.sql
 
